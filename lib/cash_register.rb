8892d59dd6748dee5c45443a3fb081cfa897e36f
Paul Holmes <paulholmes1993@gmail.com>
2019-07-14 23:58:02 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,4 +7,14 @@ class CashRegister
     @cart = []
   end
 
+  def add_item(item, price, quantity = 1)
+    @total += price * quantity
+    item_info = {}
+    item_info[:name] = item
+    item_info[:price] = price
+    item_info[:quantity] = quantity
+
+    @cart << item_info
+  end
+
   
