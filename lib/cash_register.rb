971ea9b766ceb2823ecbe8028e7bf0d9523b3238
Paul Holmes <paulholmes1993@gmail.com>
2019-07-15 00:00:36 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,4 +16,12 @@ class CashRegister
 
     @cart << item_info
   end
+
+def apply_discount
+  if @discount == 0
+    return "There is no discount to apply."
+  end
+  @total -= @total * @discount / 100
+  
+
 end
